85da7d9977495d63ddba75aeb277b425ba8bcdd4
zeyus <zeyus@zeyus.com>
2026-03-10 17:36:00 +0100

    update everything, change loading method

--- src/components/Scene.svelte
+++ src/components/Scene.svelte
@@ -1,30 +1,63 @@
 <script lang="ts">
-    import { T, useThrelte } from '@threlte/core'
-    import { useGltf, OrbitControls } from '@threlte/extras'
-    import { Color } from 'three'
+	import { T, useThrelte } from '@threlte/core';
+	import { useGltf, OrbitControls, useDraco } from '@threlte/extras';
+	import { Color, Material, Mesh } from 'three';
 
-    const { scene } = useThrelte()
-    scene.background = new Color(0x100010)
+	const { scene } = useThrelte();
+	scene.background = new Color(0x100010);
+	const dracoLoader = useDraco();
+
+	type GLTFResult = {
+		nodes: {
+			Lobe_Lobe_0: Mesh;
+			Lobe_Cerebllum_1_0: Mesh;
+			Lobe_Cerebllum_2_0: Mesh;
+		};
+		materials: {
+			Lobe: Material;
+			Cerebllum_1: Material;
+			Cerebllum_2: Material;
+		};
+	};
+
+	const gltf = useGltf<GLTFResult>('/models/scene.glb', {
+		dracoLoader
+	});
 </script>
-{#await useGltf('/models/scene.gltf') then gltf}
-    <T.PerspectiveCamera
-        makeDefault
-        position={[3, 1, 3]}
-        on:create={({ ref }) => {
-        ref.lookAt(0, 0, 0)
-        }}
-    >
-        <OrbitControls
-            autoRotate
-            autoRotateSpeed={0.7}
-            enableDamping
-            enableZoom={true}
-            target={[0, -0.2, 0]}
-        />
-    </T.PerspectiveCamera>
-    <T.AmbientLight intensity={0.25} />
-    <T.DirectionalLight position={[0, 10, 0]} intensity={1} castShadow />
-    <T is={gltf.scene} castShadow position={[-0.6, -1.0, 0]} />
-    <T.GridHelper args={[20, 20, 0xff0000, 'teal']} rotation={[Math.PI / 2, 0, Math.PI / 2]} />
-    <T.GridHelper args={[20, 20, 0xff0000, 'teal']} />
-{/await}
+
+<T.Group dispose={false}>
+	{#await gltf then gltf}
+		<T.PerspectiveCamera
+			makeDefault
+			position={[3, 1, 3]}
+			on:create={({ ref }) => {
+				ref.lookAt(0, 0, 0);
+			}}
+		>
+			<OrbitControls
+				autoRotate
+				autoRotateSpeed={0.7}
+				enableDamping
+				enableZoom={true}
+				target={[0, -0.2, 0]}
+			/>
+		</T.PerspectiveCamera>
+		<T.AmbientLight intensity={0.8} />
+		<T.DirectionalLight position={[2, 1, -2]} intensity={3.0} castShadow target={gltf.scene} />
+		<T.GridHelper args={[20, 20, 0xff0000, 'teal']} rotation={[Math.PI / 2, 0, Math.PI / 2]} />
+		<T.GridHelper args={[20, 20, 0xff0000, 'teal']} />
+		<T.Group scale={0.01} castShadow position={[-0.6, -1.0, 0]}>
+			<T.Mesh geometry={gltf.nodes.Lobe_Lobe_0.geometry} material={gltf.materials.Lobe} />
+			<T.Mesh
+				geometry={gltf.nodes.Lobe_Cerebllum_1_0.geometry}
+				material={gltf.materials.Cerebllum_1}
+			/>
+			<T.Mesh
+				geometry={gltf.nodes.Lobe_Cerebllum_2_0.geometry}
+				material={gltf.materials.Cerebllum_2}
+			/>
+		</T.Group>
+	{:catch err}
+		<p>Error loading scene: {err}</p>
+	{/await}
+</T.Group>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,90 @@
 <script lang="ts">
-  import { Canvas } from '@threlte/core'
-  import Scene from '../components/Scene.svelte'
+	import Scene from '../components/Scene.svelte';
+	import { Canvas } from '@threlte/core';
+	import { Tween } from 'svelte/motion';
+	import { fade } from 'svelte/transition';
+	import { fromStore } from 'svelte/store';
+	import { useProgress } from '@threlte/extras';
+
+	const { progress, item, loaded, total } = useProgress();
+	const p = fromStore(progress);
+	const i = fromStore(item);
+	const l = fromStore(loaded);
+	const t = fromStore(total);
+
+	const tweenedProgress = Tween.of(() => p.current, {
+		duration: 150
+	});
+
+	const progressWidth = $derived(100 * tweenedProgress.current);
+	const progressLessThanOne = $derived(tweenedProgress.current < 1);
 </script>
+
 <svelte:head>
-    <title>Brainjammers: Neurodisruptors 4 U</title> 
+	<title>Neurowang: Neurodisruptors 4 U</title>
 </svelte:head>
-<div>
-    <Canvas>
-    <Scene />
-    </Canvas>
+{#if progressLessThanOne}
+	<div
+		transition:fade={{
+			duration: 200
+		}}
+		class="wrapper"
+	>
+		<p class="loading">Loading {i ? i.current : ''} ({l.current}/{t.current})...</p>
+		<div class="bar-wrapper">
+			<div class="bar" style="width: {progressWidth}%"></div>
+		</div>
+	</div>
+{/if}
+
+<div class="main">
+	<Canvas>
+		<Scene />
+	</Canvas>
 </div>
 
 <style>
-    div {
-      height: 100vh;
-    }
+	div.main {
+		height: 100vh;
+	}
+	.wrapper {
+		position: absolute;
+		width: 100vw;
+		height: 100vh;
+		top: 0;
+		left: 0;
+		background-color: #100010;
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		align-items: center;
+		justify-content: center;
+		color: white;
+	}
+
+	.loading {
+		font-size: 2rem;
+		line-height: 4rem;
+		font-family:
+			'SF Mono',
+			SF Mono,
+			SFMono-Regular,
+			Consolas,
+			'Liberation Mono',
+			Menlo,
+			Courier,
+			monospace;
+	}
+
+	.bar-wrapper {
+		width: 33.333333%;
+		height: 20px;
+		border: 1px solid white;
+		position: relative;
+	}
+
+	.bar {
+		height: 100vh;
+		background-color: teal;
+	}
 </style>
